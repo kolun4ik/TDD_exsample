aaa66b531a059a1006fccdc8447f6ce2307a3dc7
kolun4ik <11_ka@rambler.ru>
2019-05-05 13:04:33 +0300

    Скорректировано новое представление списка с учетом валидации модели

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 from lists.models import Item, List
 
-class ListAndItemModelTest(TestCase):
-    """тест модели элемента списка"""
+class ListAndItemModelsTest(TestCase):
+    """тест модели списка и элемента списка"""
 
     def test_saving_and_retrieving_items(self):
         """тест сохранения и получения элемента списка"""
@@ -31,6 +32,14 @@ class ListAndItemModelTest(TestCase):
         self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
         self.assertEqual(second_saved_item.list, list_)
+
+    def test_cannot_save_empty_list_items(self):
+        """тест: нельзя добавить пустые элементы списка"""
+        list_ = List.objects.create()
+        item = Item(list=list_, text='')
+        with self.assertRaises(ValidationError):
+            item.save()
+            item.full_clean()
 
 
 class ListViewTest(TestCase):
