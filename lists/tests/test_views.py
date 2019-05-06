b13f32f0bf4665eae7adc7f77c31b3690488515a
kolun4ik <11_ka@rambler.ru>
2019-05-06 12:28:55 +0300

    Применена новая форма в home_page. NB нарушена логика

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.utils.html import escape
 from lists.models import Item, List
+from lists.forms import ItemForm
 
 
 class HomePageTest(TestCase):
@@ -11,6 +12,10 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
+    def test_home_page_uses_item_form(self):
+        """тест: домашняя страница использует форму для элемента"""
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
 
 class ListViewTest(TestCase):
     """тест предоставления списка"""
